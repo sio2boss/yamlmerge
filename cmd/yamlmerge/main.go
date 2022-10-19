33129ed7998c3d4a6dee70a478e69e741477b96e
sio2boss <sio2boss@hotmail.com>
2022-10-19 13:11:14 -0400

    Adding short help

--- cmd/yamlmerge/main.go
+++ cmd/yamlmerge/main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "github.com/docopt/docopt-go"
-    "gopkg.in/yaml.v2"
-    "io/ioutil"
-    "log"
-    "os"
-    "reflect"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+	"strings"
+
+	"github.com/docopt/docopt-go"
+	"gopkg.in/yaml.v2"
 )
 
 // exists returns whether the given file or directory exists
 func exists(path string) (bool, error) {
-    _, err := os.Stat(path)
-    if err == nil {
-        return true, nil
-    }
-    if os.IsNotExist(err) {
-        return false, nil
-    }
-    return true, err
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
 
 func main() {
-    // config stuff
-    usage := `YAML Merge
+	// config stuff
+	usage := `YAML Merge
 
 Usage:
   yamlmerge <input> <base> <override> 
@@ -41,116 +42,121 @@ Options:
   -h --help      Show this screen.
   --version      Show version.`
 
-    osargs := os.Args[1:]
-    arguments, _ := docopt.ParseArgs(usage, osargs, "1.0")
+	if os.Getenv("AV_SINGLE_LINE_HELP") != "" {
+		fmt.Println("Simple tool that recursively merges YAML files")
+		os.Exit(0)
+	}
 
-    var config struct {
-        Input    string `docopt:"<input>"`
-        Base     string `docopt:"<base>"`
-        Override string `docopt:"<override>"`
-        GetRoles bool   `docopt:"--get-roots"`
-    }
+	osargs := os.Args[1:]
+	arguments, _ := docopt.ParseArgs(usage, osargs, "1.0")
 
-    arguments.Bind(&config)
+	var config struct {
+		Input    string `docopt:"<input>"`
+		Base     string `docopt:"<base>"`
+		Override string `docopt:"<override>"`
+		GetRoles bool   `docopt:"--get-roots"`
+	}
 
-    inputFile := config.Input
-    defaultRole := config.Base
-    role := config.Override
-    getroles := config.GetRoles
+	arguments.Bind(&config)
 
-    // define defaults for empty values
-    if ok, err := exists(inputFile); !ok {
-        if inputFile == "" {
-            log.Fatal("No input file specified.")
-        }
-        fmt.Printf("No file found at: %s", inputFile)
-        if err != nil {
-            fmt.Println("Additionally, the following error occurred:")
-            fmt.Println(err.Error())
-        }
-        os.Exit(1)
-    }
+	inputFile := config.Input
+	defaultRole := config.Base
+	role := config.Override
+	getroles := config.GetRoles
 
-    // read the input file
-    file, err := ioutil.ReadFile(inputFile)
-    if err != nil {
-        log.Fatalf("Unable to load file: %s\n%v", inputFile, err)
-    }
-    configMap := make(map[interface{}]interface{})
-    err = yaml.Unmarshal(file, &configMap)
-    if err != nil {
-        log.Fatalf("Unable to deserialize file: %s\n%v", inputFile, err)
-    }
+	// define defaults for empty values
+	if ok, err := exists(inputFile); !ok {
+		if inputFile == "" {
+			log.Fatal("No input file specified.")
+		}
+		fmt.Printf("No file found at: %s", inputFile)
+		if err != nil {
+			fmt.Println("Additionally, the following error occurred:")
+			fmt.Println(err.Error())
+		}
+		os.Exit(1)
+	}
 
-    var roleMap, baseMap map[interface{}]interface{}
+	// read the input file
+	file, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		log.Fatalf("Unable to load file: %s\n%v", inputFile, err)
+	}
+	configMap := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(file, &configMap)
+	if err != nil {
+		log.Fatalf("Unable to deserialize file: %s\n%v", inputFile, err)
+	}
 
-    roleMap = configMap
-    //delete(roleMap, defaultRole) // take out the default
+	var roleMap, baseMap map[interface{}]interface{}
 
-    if getroles {
-        availableRoles := getStringKeys(roleMap)
-        fmt.Println(strings.Join(availableRoles, "\n"))
-        os.Exit(0)
-    }
+	roleMap = configMap
+	//delete(roleMap, defaultRole) // take out the default
 
-    baseMap = configMap[defaultRole].(map[interface{}]interface{})
+	if getroles {
+		availableRoles := getStringKeys(roleMap)
+		fmt.Println(strings.Join(availableRoles, "\n"))
+		os.Exit(0)
+	}
 
-    roleMapReflectValue := reflect.ValueOf(roleMap).MapIndex(reflect.ValueOf(role))
-    if !roleMapReflectValue.IsValid() {
-        log.Fatalf("Role %v was not found in input file %s", role, inputFile)
-    }
+	baseMap = configMap[defaultRole].(map[interface{}]interface{})
 
-    roleEnvironmentMap := roleMapReflectValue.Interface().(map[interface{}]interface{})
-    baseMapReflectValue := reflect.ValueOf(baseMap).Interface()
+	roleMapReflectValue := reflect.ValueOf(roleMap).MapIndex(reflect.ValueOf(role))
+	if !roleMapReflectValue.IsValid() {
+		log.Fatalf("Role %v was not found in input file %s", role, inputFile)
+	}
 
-    mp := merge(roleEnvironmentMap, baseMapReflectValue)
+	roleEnvironmentMap := roleMapReflectValue.Interface().(map[interface{}]interface{})
+	baseMapReflectValue := reflect.ValueOf(baseMap).Interface()
 
-    // write the output
-    yamlBytes, err := yaml.Marshal(mp)
-    if err != nil {
-        log.Fatalf("Error marshalling yaml output: ", err.Error())
-    }
-    fmt.Println(string(yamlBytes))
+	mp := merge(roleEnvironmentMap, baseMapReflectValue)
+
+	// write the output
+	yamlBytes, err := yaml.Marshal(mp)
+	if err != nil {
+		log.Fatalf("Error marshalling yaml output: ", err.Error())
+	}
+	fmt.Println(string(yamlBytes))
 }
 
 // getStringKeys returns all keys in the map provided as strings
 func getStringKeys(roleMap map[interface{}]interface{}) []string {
-    roles := make([]string, len(roleMap))
-    i := 0
-    for k := range roleMap {
-        var kString string
-        kString = k.(string)
-        roles[i] = kString
-        i++
-    }
-    return roles
+	roles := make([]string, len(roleMap))
+	i := 0
+	for k := range roleMap {
+		var kString string
+		kString = k.(string)
+		roles[i] = kString
+		i++
+	}
+	return roles
 }
 
 // recursively merges two maps; assumes the second map is a superset of the first
 func merge(role interface{}, app interface{}) map[interface{}]interface{} {
-    outmap := make(map[interface{}]interface{})
+	outmap := make(map[interface{}]interface{})
 
-    roleValue := reflect.ValueOf(role).Interface().(map[interface{}]interface{})
-    appValue := reflect.ValueOf(app).Interface().(map[interface{}]interface{})
+	roleValue := reflect.ValueOf(role).Interface().(map[interface{}]interface{})
+	appValue := reflect.ValueOf(app).Interface().(map[interface{}]interface{})
 
-    // for all in role that are also in app, recur downward and replace crap
-    for k, v := range appValue {
-        roleMapValue, ok := roleValue[k]
-        if !ok {
-            outmap[k] = v
-        } else if reflect.ValueOf(v).Kind() == reflect.Map {
-            outmap[k] = merge(roleMapValue, v)
-        } else {
-            outmap[k] = roleMapValue
-        }
-    }
+	// for all in role that are also in app, recur downward and replace crap
+	for k, v := range appValue {
+		roleMapValue, ok := roleValue[k]
+		if !ok {
+			outmap[k] = v
+		} else if reflect.ValueOf(v).Kind() == reflect.Map {
+			outmap[k] = merge(roleMapValue, v)
+		} else {
+			outmap[k] = roleMapValue
+		}
+	}
 
-    // for all in role that are not also in app, just take the whole tree
-    for k, v := range roleValue {
-        _, ok := appValue[k]
-        if !ok {
-            outmap[k] = v
-        }
-    }
-    return outmap
+	// for all in role that are not also in app, just take the whole tree
+	for k, v := range roleValue {
+		_, ok := appValue[k]
+		if !ok {
+			outmap[k] = v
+		}
+	}
+	return outmap
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.12
 
 require (
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
-	gopkg.in/yaml.v2 v2.2.2
+	gopkg.in/yaml.v2 v2.4.0
 )
